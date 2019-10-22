203fb8be390695d22b553406518f6e083b723abf
Student <Student@STUDENT-PC12>
2019-10-22 17:20:26 -0500

    Modulos

--- OoapasNewDemo/OoapasNewDemo/OoapasNewDemo.Web/Modules/Common/Navigation/NavigationItems.cs
+++ OoapasNewDemo/OoapasNewDemo/OoapasNewDemo.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -2,3 +2,17 @@
 using Administration = OoapasNewDemo.Administration.Pages;
 
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+[assembly: NavigationLink(1001, "Modulo Comercial", url: "", permission: "", icon: "")]
+[assembly: NavigationLink(1002, "Modulo Comercial/Padron", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1003, "Modulo Comercial/Contratos", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1004, "Modulo Comercial/Hidrometria", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1005, "Modulo Comercial/Facturacion", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1006, "Modulo Comercial/Servicios", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1007, "Modulo Comercial/Gestion Cobranza", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1008, "Modulo Comercial/Recaudacion", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1009, "Modulo Comercial/Analisis General", url: "~/", permission: "", icon: "")]
+
+[assembly: NavigationLink(1010, "Modulo Operacion", url: "", permission: "", icon: "")]
+[assembly: NavigationLink(1011, "Modulo Operacion/Padron", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1012, "Modulo Operacion/Contratos", url: "~/", permission: "", icon: "")]
+[assembly: NavigationLink(1013, "Modulo Operacion/Hidrometria", url: "~/", permission: "", icon: "")]
---
+++ OoapasNewDemo/OoapasNewDemo/OoapasNewDemo.Web/Modules/ModuloOperacion/Factibilidades/FactibilidadesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ooapas.Web.Modules.ModuloOperacion.Factibilidades
+{
+    public class FactibilidadesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
