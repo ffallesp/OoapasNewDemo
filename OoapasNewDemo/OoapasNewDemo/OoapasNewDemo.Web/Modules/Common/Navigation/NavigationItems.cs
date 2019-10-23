ee8289c0b5519f61e4ba10377e35625dc705be98
Student <Student@STUDENT-PC12>
2019-10-23 15:44:55 -0500

    inicio

--- OoapasNewDemo/OoapasNewDemo/OoapasNewDemo.Web/Modules/Common/Navigation/NavigationItems.cs
+++ OoapasNewDemo/OoapasNewDemo/OoapasNewDemo.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -1,4 +1,4 @@
 ﻿using Serenity.Navigation;
 using Administration = OoapasNewDemo.Administration.Pages;
 
-[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+[assembly: NavigationLink(1000, "Inicio", url: "~/", permission: "", icon: "fa-tachometer")]
